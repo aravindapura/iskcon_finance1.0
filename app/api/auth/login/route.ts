a8b8c862d379949c86f599e996a6207e8817bcfe
aravindapura <anatolysmolin@gmail.com>
2025-10-10 01:12:40 +0400

    Update route.ts

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,6 +1,18 @@
+// app/api/auth/login/route.ts
+import { NextResponse } from "next/server";
+
 export const runtime = "nodejs";
 
+// Временный вход без авторизации
 export const POST = async () => {
-  const user = { id: "0001", login: "buh", role: "admin" };
-  return NextResponse.json({ token: "mock-token", user });
+  const user = {
+    id: "00000000-0000-0000-0000-000000000001",
+    login: "buh",
+    role: "admin",
+  };
+
+  return NextResponse.json({
+    token: "mock-token",
+    user,
+  });
 };
